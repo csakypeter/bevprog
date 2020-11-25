24c0d6f70a1abe24c596e402ef89e65671018cb8
Track <csakypeters@gmail.com>
2020-11-25 20:20:46 +0100

    Uploaded exercises with memory

---
+++ lab/memory/mem_002.cpp
@@ -0,0 +1,13 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+	cout << "Size of char: " << sizeof(char) << " byte.\n";
+	cout << "Size of int: " << sizeof(int) << " byte.\n";
+	cout << "Size of double: " << sizeof(double) << " byte.\n";
+	cout << "Size of char*: " << sizeof(char*) << " byte.\n";
+	cout << "Size of int*: " << sizeof(int*) << " byte.\n";
+	cout << "Size of double*: " << sizeof(double*) << " byte.\n";
+
+	return 0;
+}
